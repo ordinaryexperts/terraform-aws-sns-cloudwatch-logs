aedf7c11dfa6dd1ac21617e30a462a1b04acb6be
Jason McVetta <jmcvetta@hey.com>
2025-06-25 13:22:04 +0700

    remove invalid output

--- outputs.tf
+++ outputs.tf
@@ -9,7 +9,8 @@ output "lambda_name" {
 
 output "lambda_arn" {
   description = "ARN of created Lambda Function."
-  value       = var.lambda_publish_func ? aws_lambda_function.sns_cloudwatchlog.qualified_arn : aws_lambda_function.sns_cloudwatchlog.arn
+  value       = var.lambda_publish_func ? aws_lambda_function.sns_cloudwatchlog.qualified_arn :
+    aws_lambda_function.sns_cloudwatchlog.arn
 }
 
 output "lambda_version" {
@@ -49,16 +50,11 @@ output "log_group_name" {
 
 output "log_group_arn" {
   description = "ARN of CloudWatch Log Group."
-  value       = var.create_log_group ? aws_cloudwatch_log_group.sns_logged_item_group[0].arn : data.aws_cloudwatch_log_group.sns_logged_item_group[0].arn
-}
-
-output "log_stream_arn" {
-  description = "ARN of CloudWatch Log Stream."
-  value       = var.create_sns_topic ? aws_sns_topic.sns_log_topic[0].arn : data.aws_sns_topic.sns_log_topic[0].arn
+  value       = var.create_log_group ? aws_cloudwatch_log_group.sns_logged_item_group[0].arn :
+    data.aws_cloudwatch_log_group.sns_logged_item_group[0].arn
 }
 
 output "cloudwatch_event_rule_arn" {
   description = "ARN of CloudWatch Trigger Event created to prevent hibernation."
   value       = var.create_warmer_event ? aws_cloudwatch_event_rule.warmer[0].arn : ""
 }
-
